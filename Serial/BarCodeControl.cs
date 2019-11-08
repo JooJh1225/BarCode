b2abb53b303dbbb10dfc9a65d61afe7f55371660
JooJh1225 <56584939+JooJh1225@users.noreply.github.com>
2019-11-08 21:51:00 +0900

    3

--- Serial/BarCodeControl.cs
+++ Serial/BarCodeControl.cs
@@ -6,22 +6,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
-using QRCoder; 
+using QRCoder;
+using ZXing;
 
 namespace BarCode
 {
     class BarCodeControl
     {
-        public BitmapImage GenBarCode(string src)
+        public BitmapImage GenBarCode(string content,BarcodeFormat barcodeFormat )
         {
             //바코드 생성
-            QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            QRCodeData qrData = qrGenerator.CreateQrCode(src, QRCodeGenerator.ECCLevel.Q);
-            QRCode qrCode = new QRCode(qrData);
-            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            BarcodeWriter writer = new BarcodeWriter();
+
+            writer.Format = barcodeFormat;
+            writer.Options = new ZXing.Common.EncodingOptions
+            {
+                Height = 1000,
+                Width = 1000,
+
+            };
+            Bitmap barcodeImage = writer.Write(content);
             //비트맵 변환
             MemoryStream ms = new MemoryStream();
-            ((System.Drawing.Bitmap)qrCodeImage).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            ((System.Drawing.Bitmap)barcodeImage).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
             BitmapImage image = new BitmapImage();
             image.BeginInit();
             ms.Seek(0, SeekOrigin.Begin);
--- Serial/MainViewModel.cs
+++ Serial/MainViewModel.cs
@@ -1,24 +1,50 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using System.Collections.Generic;
+using ZXing;
 
 namespace BarCode
 {
     internal class MainViewModel : INotifyPropertyChanged
     {
+
+
         public DispatcherTimer timer = new DispatcherTimer();
 
         public BarCodeControl bc = new BarCodeControl();
 
         public RandomStream rs = new RandomStream();
 
+        private List<Tuple<BarcodeFormat, string>> barcodeFormats = new List<Tuple<BarcodeFormat, string>>()
+        {
+            new Tuple<BarcodeFormat, string>(BarcodeFormat.QR_CODE, "QRCode"),
+            new Tuple<BarcodeFormat, string>(BarcodeFormat.CODE_128, "CODE128"),
+            new Tuple<BarcodeFormat, string>(BarcodeFormat.CODE_39, "CODE39")
+            //BarcodeFormat.CODE_128,
+            //BarcodeFormat.EAN_13,
+            //BarcodeFormat.CODE_39
+        };
+
         private ObservableCollection<model> barCodeList = new ObservableCollection<model>();
 
         private Double timerDouble;
+
+        private int barcodeLength;
+
+        private BarcodeFormat selectedBarcode;
+
+        public List<Tuple<BarcodeFormat, string>> BarcodeFormats
+        {
+            get { return barcodeFormats; }
+            set
+            {
+                barcodeFormats = value;
+            }
+        }
 
         public ObservableCollection<model> BarCodeList
         {
@@ -40,6 +66,26 @@ namespace BarCode
             }
         }
 
+        public int BarcodeLength
+        {
+            get { return barcodeLength; }
+            set
+            {
+                barcodeLength = value;
+                OnPropertyChanged(nameof(BarcodeLength));
+            }
+        }
+
+        public BarcodeFormat SelectedBarcode
+        {
+            get { return selectedBarcode; }
+            set
+            {
+                selectedBarcode = value;
+                OnPropertyChanged(nameof(SelectedBarcode));
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged(string propertyName)
@@ -50,27 +96,35 @@ namespace BarCode
             }
         }
 
+       
+
         public MainViewModel()
         {
             this.ButtonClk = new ButtonClk(this);
+            this.StopTimer = new StopTimer(this);
         }
 
         public ICommand ButtonClk { protected get; set; }
+        public ICommand StopTimer { protected get; set; }
 
-        public void aa()
+        public void aa(int length)
         {
-            string rnd = rs.rnd(8);
+            string rnd = rs.rnd(length);
             model md = new model();
-            BitmapImage qrcode = bc.GenBarCode(rnd);
+            BitmapImage qrcode = bc.GenBarCode(rnd, SelectedBarcode);
             md.Bitimg = qrcode;
             md.BarCodeText = rnd;
             BarCodeList.Add(md);
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+           aa(BarcodeLength);
         }
     }
 
     internal class ButtonClk : ICommand
     {
-
         public event EventHandler CanExecuteChanged;
 
         private MainViewModel viewModel;
@@ -89,13 +143,33 @@ namespace BarCode
         {
             viewModel.OnPropertyChanged(nameof(viewModel.TimerDouble));
             viewModel.timer.Interval = TimeSpan.FromMilliseconds((double)parameter);
-            viewModel.timer.Tick += new EventHandler(timer_Tick);
+            viewModel.timer.Tick += new EventHandler(viewModel.timer_Tick);
             viewModel.timer.Start();
         }
 
-        private void timer_Tick(object sender, EventArgs e)
+        
+    }
+
+    internal class StopTimer : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private MainViewModel viewModel;
+
+        public StopTimer(MainViewModel viewModel)
         {
-            viewModel.aa();
+            this.viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.timer.Tick -= new EventHandler(viewModel.timer_Tick);
+            viewModel.timer.Stop();            
         }
     }
 }
